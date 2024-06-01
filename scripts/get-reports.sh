4d74c48f9176ba8c31435d1dc41eba325f1bf1fa
David Kopp <git@davidkopp.de>
2024-06-01 11:21:24 +0200

    minor

--- scripts/get-reports.sh
+++ scripts/get-reports.sh
@@ -6,4 +6,4 @@ MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ssh david@davidkopp.de 'bash -s' < $MY_DIR/generate-reports.sh
 
 mkdir -p $MY_DIR/../reports/
-rsync -aP david@davidkopp.de:/tmp/reports $MY_DIR/../reports
+rsync -aP david@davidkopp.de:/tmp/reports/ $MY_DIR/../reports
