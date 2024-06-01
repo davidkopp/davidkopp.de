d2edb11e66031d972b856594df2c856a5e4f6d9b
David Kopp <git@davidkopp.de>
2024-06-01 09:11:52 +0200

    Fix deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 set -e
 
+mkdir -p public
 cp index.* public/
 
 echo "Sending to remote"
-rsync -ru public/. david@davidkopp.de:/var/www/html --delete
+rsync -ru public/. david@davidkopp.de:/var/www/www.davidkopp.de/
 
 echo "Finished."
